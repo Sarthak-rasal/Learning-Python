293589ab472c3613c58c0d6815494be51823bbe5
Sarthak Rasal <varshanr75@gmail.com>
2025-03-06 18:58:57 +0530

    added main

---
+++ 1st Hour/main.py
@@ -0,0 +1,37 @@
+#This is my first comment
+
+print("Hello World")
+print("I love burgers")
+
+
+#Strings
+first_name = "Bro"
+food = "Pizza"
+
+print(first_name)
+print(f"Hello {first_name}")
+
+#Integers
+age = 25
+quantity = 3
+
+print(f"you are {age}")
+print(f"you are buying {quantity} items")
+
+#Float
+
+price = 10.99
+
+print(f"The price is ${price}")
+
+#Boolean
+
+is_student = True
+
+print(f"Are you a student {is_student}")
+if is_student:
+    print("you are a student")
+else:
+    print("you are not a student")
+
+
